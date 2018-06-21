662629fc2321ba519b1b51640cc7069644f99500
yangya <759106909@qq.com>
2018-06-21 17:56:20 +0800

    new chapter2_1

---
+++ src/main/java/edu/princeton/cs/exercise/chapter2_1/Selection.java
@@ -0,0 +1,49 @@
+package edu.princeton.cs.exercise.chapter2_1;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Selection {
+
+    public static void sort(Comparable[] a)
+    {
+
+    }
+
+    /***************************************************************************
+     *  Helper sorting functions.
+     ***************************************************************************/
+    private static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j)
+    {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean isSorted(Comparable[] a)
+    {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    private static void show(Comparable[] a)
+    {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
